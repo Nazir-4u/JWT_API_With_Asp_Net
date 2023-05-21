a4924772d9ba3654214f5711fd7acb725832dad4
MOHHAMAD NAZIR AHMAD <54079449+Nazir-4u@users.noreply.github.com>
2023-05-22 00:00:45 +0530

    Add files via upload

---
+++ NAZIR_API_JWT/Controllers/LoginController.cs
@@ -0,0 +1,64 @@
+﻿// LoginController.cs
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace JWT.Controllers
+{
+	public class UserModel
+	{
+		public string username { get; set; }
+		public string password { get; set; }
+		public string email { get; set; }
+	}
+
+	[ApiController]
+	[Route("api/[controller]")]
+	public class LoginController : ControllerBase
+	{
+		[Route("CreateJWT")]
+		private string CreateJWT(UserModel user)
+		{
+			var secretkey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("yJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY4NDY5MDk3MywiaWF0IjoxNjg0NjkwOTczfQ.Vq6MzQTC7jhGmyr7QeXjfaGyghZvRYu624AtS3o8C20")); // NOTE: SAME KEY AS USED IN Startup.cs FILE
+			var credentials = new SigningCredentials(secretkey, SecurityAlgorithms.HmacSha256);
+
+			var claims = new[] // NOTE: could also use List<Claim> here
+			{
+				new Claim(ClaimTypes.Name, user.username), // this will be "User.Identity.Name" value
+				new Claim(JwtRegisteredClaimNames.Sub, user.username),
+				new Claim(JwtRegisteredClaimNames.Email, user.email),
+				new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString("N")) // this could the unique ID assigned to the user by a database
+			};
+
+			var token = new JwtSecurityToken(issuer: "https://localhost:44345/", audience: "https://localhost:44345/", claims: claims, expires: DateTime.Now.AddMinutes(60), signingCredentials: credentials);
+			return new JwtSecurityTokenHandler().WriteToken(token);
+		}
+
+		private UserModel Authenticate(UserModel login)
+		{
+			if (login.username == "test" && login.password == "abc123") // NOTE: in production, query a database for user information
+				return new UserModel { username = login.username, email = "test@gmail.com" };
+			return null;
+		}
+
+		[HttpPost,Route("Post")]
+		public async Task<IActionResult> Post([FromBody] UserModel login)
+		{
+			return await Task.Run(() =>
+			{
+				IActionResult response = Unauthorized();
+
+				UserModel user = Authenticate(login);
+
+				if (user != null)
+					response = Ok(new { token = CreateJWT(user) });
+
+				return response;
+			});
+		}
+	}
+}
---
+++ NAZIR_API_JWT/Startup.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+namespace NAZIR_API_JWT
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            // NOTE: the following lines are newly added
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = true,
+                    ValidAudience = "domain.com",
+                    ValidateIssuer = true,
+                    ValidIssuer = "domain.com",
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("THIS IS THE SECRET KEY")) // NOTE: THIS SHOULD BE A SECRET KEY NOT TO BE SHARED
+                };
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "NAZIR_API_JWT", Version = "v1" });
+            });
+
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NAZIR_API_JWT v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
